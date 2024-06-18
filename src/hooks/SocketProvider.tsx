97f0018d42c6fc948c4e5101576afae818297f8d
Ozod <o.bukhorizoda@mail.ru>
2024-06-18 22:51:52 +0500

    changes

--- src/hooks/SocketProvider.tsx
+++ src/hooks/SocketProvider.tsx
@@ -10,7 +10,7 @@ interface SocketContextType {
 }
 
 export const SocketContext = createContext<SocketContextType | null>(null);
-const socket = io("https://socket-io-server-4unqhu1ro-itsozods-projects.vercel.app/");
+const socket = io("https://socket-io-server-t5eb.onrender.com");
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
   const [messages, setMessages] = useState<string[]>([]);
