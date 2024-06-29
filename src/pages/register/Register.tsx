14d4a4cbaca9e030b52ab77df71935e8a147bb53
Ozod <o.bukhorizoda@mail.ru>
2024-06-29 23:14:56 +0500

    chore: add font

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -55,7 +55,17 @@ const Register = () => {
         <div className="flex justify-center items-center flex-col h-[100vh] g-10">
           <div className="w-[100%] max-w-[300px] p-5 rounded-md border border-red-500">
             <div className="flex items-center flex-col gap-2">
-              <h1 className="text-[#f4f4f4] text-3xl">ChitChat</h1>
+              <h1
+                style={{
+                  fontFamily: "Playwrite IE, cursive",
+                  fontOpticalSizing: "auto",
+                  fontWeight: "bold",
+                  fontStyle: "normal",
+                }}
+                className="text-[#f4f4f4] text-3xl"
+              >
+                next chat
+              </h1>
               <p className="text-[#7B7B7B]">Please Sign In to your account</p>
             </div>
 
